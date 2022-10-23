4ba3848528a7be2f757049d4a21fb39c6fbd12bd
samishakhurana <samishak62@gmail.com>
2022-10-23 15:31:48 -0700

    deployed URL added for mock server

--- MyLibrary/Tests/MyLibraryTests/WeatherServiceTest.swift
+++ MyLibrary/Tests/MyLibraryTests/WeatherServiceTest.swift
@@ -3,18 +3,18 @@ import MyLibrary
 
 final class WeatherServiceTests: XCTestCase {
     
-//    func testweatherSeriveTest_SuccessCase() async throws {
-//        // Given
-//        let returnedVal = 10
-//        let localPath = "http://127.0.0.1:3000/"
-//        let weatherSerivce = WeatherServiceImpl(baseUrl: localPath)
-//        
-//        // When
-//        let temperature = try await weatherSerivce.getTemperature()
-//        print(temperature)
-//        
-//        // Then
-//        XCTAssertNotNil(temperature)
-//        XCTAssertEqual(temperature, Int(returnedVal))
-//    }
+    func testweatherSeriveTest_SuccessCase() async throws {
+        // Given
+        let returnedVal = 38
+        let localPath = "https://weather-api-mock-samishakhurana.vercel.app"
+        let weatherSerivce = WeatherServiceImpl(baseUrl: localPath)
+        
+        // When
+        let temperature = try await weatherSerivce.getTemperature()
+        print("debugging temperature ", temperature)
+        
+        // Then
+        XCTAssertNotNil(temperature)
+        XCTAssertEqual(temperature, Int(returnedVal))
+    }
 }
